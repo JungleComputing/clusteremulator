9dace9452dbb387e121e3f5438b64b85ecc264f9
Mathijs den Burger <mdenburger@gmail.com>
2009-03-10 13:28:02 +0000

    - renamed tc_* scripts to tc-* - fixed tc-clean.sh

--- bin/tc-clean.sh
+++ bin/tc-clean.sh
@@ -6,7 +6,7 @@
 if [ "$(hostname -f)" != "fs0.das3.cs.vu.nl" ]; then
     interfaces=$(/sbin/tc qdisc | awk '{print $5}')
     
-    for if in "$interfaces"; do
+    for if in $interfaces; do
         sudo /sbin/tc qdisc del dev $if root >/dev/null 2>&1
     done
 fi
