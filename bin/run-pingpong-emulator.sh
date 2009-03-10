603e193d47beb9c5ffcc84fdf47d2d20d83baf1e
Mathijs den Burger <mdenburger@gmail.com>
2009-03-10 13:51:25 +0000

    fixed the educational check :)

--- bin/run-pingpong-emulator.sh
+++ bin/run-pingpong-emulator.sh
@@ -19,7 +19,7 @@ ibis_server_port=44661
 poolinfo_server_port=44662
 
 # Check if you have created a ~/.sge_epilog file (see README.txt)
-if [ ! -x $USER/.sge_epilog ]; then
+if [ ! -x ~/.sge_epilog ]; then
     echo "Please create an executable file '~/.sge_epilog' to cleanup LTC settings."
     echo "For more information, see README.txt, Section 6 'Cleaning up'."
     exit 1
